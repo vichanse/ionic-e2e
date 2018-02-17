63f888e38f71df82fcacf8589c0af1ea66c3a32b
Vicky <vicky.nseng@gmail.com>
2018-02-17 05:12:32 +0100

    Finishing the navigation system

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,10 +7,10 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
   menuData = [
-    { title: 'Our Menu', pic:'assets/img/soup1.jpg', pushPgae: 'MenuPage'},
-    { title: 'Account', pic:'assets/img/coffee-people3.jpg', pushPgae: 'AccountPage'},
-    { title: 'About Us', pic:'assets/img/coffee6.jpg', pushPgae: 'AboutPage'},
-    { title: 'Locations', pic:'assets/img/cafe2.jpg', pushPgae: 'LocationsPage'},
+    { title: 'Our Menu', pic:'assets/img/soup1.jpg', pushPage: 'MenuPage'},
+    { title: 'Account', pic:'assets/img/coffee-people3.jpg', pushPage: 'AccountPage'},
+    { title: 'About Us', pic:'assets/img/coffee6.jpg', pushPage: 'AboutPage'},
+    { title: 'Locations', pic:'assets/img/cafe2.jpg', pushPage: 'LocationsPage'},
   ];
 
   constructor(public navCtrl: NavController) {
