682124c82f7b7d501f9e6ce4f3ca92e9c2bb8b44
Vicky <vicky.nseng@gmail.com>
2018-02-17 05:40:54 +0100

    The login Page

--- src/pages/register/register.ts
+++ src/pages/register/register.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 /**
- * Generated class for the LoginPage page.
+ * Generated class for the RegisterPage page.
  *
  * See https://ionicframework.com/docs/components/#navigation for more info on
  * Ionic pages and navigation.
@@ -10,19 +10,16 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 @IonicPage()
 @Component({
-  selector: 'page-login',
-  templateUrl: 'login.html',
+  selector: 'page-register',
+  templateUrl: 'register.html',
 })
-export class LoginPage {
-
-  regPage: any;
+export class RegisterPage {
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
-    this.regPage = 'RegisterPage';
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad LoginPage');
+    console.log('ionViewDidLoad RegisterPage');
   }
 
 }
