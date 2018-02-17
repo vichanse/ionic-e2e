a5d37f59cde3d90081b821d752a2511e542d2aba
Vicky <vicky.nseng@gmail.com>
2018-02-17 04:53:50 +0100

    Configuring the Splach Page and Creating the Navigation Menu

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,6 +6,12 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  menuData = [
+    { title: 'Our Menu', pic:'assets/img/soup1.jpg', pushPgae: 'MenuPage'},
+    { title: 'Account', pic:'assets/img/coffee-people3.jpg', pushPgae: 'AccountPage'},
+    { title: 'About Us', pic:'assets/img/coffee6.jpg', pushPgae: 'AboutPage'},
+    { title: 'Locations', pic:'assets/img/cafe2.jpg', pushPgae: 'LocationsPage'},
+  ];
 
   constructor(public navCtrl: NavController) {
 
