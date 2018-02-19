2a6dba4fa7ac227ec92bbe2515cc33e568511705
Vicky <vicky.nseng@gmail.com>
2018-02-19 04:47:24 +0100

    Configuring the rewards service art 2

--- src/providers/reward-service/reward-service.ts
+++ src/providers/reward-service/reward-service.ts
@@ -3,12 +3,18 @@ import { Injectable } from '@angular/core';
 import 'rxjs/add/operator/map';
 import Promise from 'promise-polyfill';
 
+import { Storage } from '@ionic/storage';
+
 @Injectable()
 export class RewardServiceProvider {
 
-  constructor() {
+  constructor( private storage: Storage) {
     console.log('Hello RewardServiceProvider Provider');
   }
+
+  rewards: any[] = [];
+
+  list: any[] = [.50, .75, 1.00, 1.25, 1.50, 1.75, 2.00, .25, .50, .75];
 
   rewardsCheck(user, userData) {
     return new Promise((resolve, reject) => {
@@ -23,6 +29,49 @@ export class RewardServiceProvider {
         resolve(userData);
       } else {
         resolve(userData);
+      }
+    })
+  }
+
+  rewardChance(user, count) {
+    if (count == 0) {
+      count++;
+      this.generateReward(user, count);
+      return count;
+    } else {
+      let chance = Math.floor((Math.random() * 100) + 1);
+
+      if (chance > 50 ) {
+        count ++;
+        this.generateReward(user, count);
+        return count;
+      } else {
+        return count;
+      }
+    }
+  }
+
+  generateReward(user, count) {
+    let dex = Math.floor((Math.random() * 10));
+    let rewarded = this.list[dex];
+
+    let rewardObj = {
+      rewardId: `REW-${count}`,
+      amount: rewarded
+    }
+
+    this.storage.get(`${user}-rewards`)
+    .then( returned => {
+
+      if (!returned) {
+        this.rewards.push(rewardObj);
+        this.storage.set(`${user}-rewards`, this.rewards)
+        .then(res => console.log(user, `Awarded ${rewarded}`));
+      } else {
+        this.rewards = returned;
+        this.rewards.push(rewardObj);
+        this.storage.set(`${user}-rewards`, this.rewards)
+        .then(res => console.log(user, `Awarded ${rewarded}`));
       }
     })
   }
